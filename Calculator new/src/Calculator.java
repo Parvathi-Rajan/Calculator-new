d153207382f52c42914c3b47d109f0b7ad27e6d0
Parvathi Rajan <rajanparvathi839@gmail.com>
2024-02-23 15:29:03 +0530

    Font style Edited.

--- Calculator new/src/Calculator.java
+++ Calculator new/src/Calculator.java
@@ -16,7 +16,7 @@ public class Calculator implements ActionListener {
 	JPanel panel;
 	
 	
-	Font myFont = new Font("Ink Free",Font.BOLD,30);
+	Font myFont = new Font("Arial",Font.BOLD,30);
 	 double num1=0,num2=0,result=0;
 	 char operator;
 	
